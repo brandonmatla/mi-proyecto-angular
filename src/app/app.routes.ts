9d54350b1701cc1cabed770fc97cec37eb4dba52
Brandon Matla <brandonmatla@gmail.com>
2025-03-15 19:04:44 -0600

    Descripción de los cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 import { PsicologoComponent } from './componentes/psicologo/psicologo.component';
+import { PreciosComponent } from './componentes/precios/precios.component';
 
 export const routes: Routes = [
     {path: '',component: InicioComponent},
-    {path: 'psicologos',component: PsicologoComponent}
+    {path: 'psicologos',component: PsicologoComponent},
+    {path: 'precios',component: PreciosComponent}
 ];
