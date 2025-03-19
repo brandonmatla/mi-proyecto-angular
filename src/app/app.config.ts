7c8b4112ddaf7f745704099140ce245e3d531904
Brandon Matla <brandonmatla@gmail.com>
2025-03-19 14:02:57 -0600

    Actualización del código fuente

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import {provideAnimations} from '@angular/platform-browser/animations'
 
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { providePrimeNG } from 'primeng/config';
-import Material from '@primeng/themes/Material';
 
 export const appConfig: ApplicationConfig = {
   providers: [
