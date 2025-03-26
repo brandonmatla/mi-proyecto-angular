ca87dd493f04b515c3f261fbb8848ba6956ee9a2
Brandon Matla <brandonmatla@gmail.com>
2025-03-25 18:33:08 -0600

    Descripción de los cambios

--- src/app/componentes/psicologo/psicologo.component.ts
+++ src/app/componentes/psicologo/psicologo.component.ts
@@ -7,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './psicologo.component.css'
 })
 export class PsicologoComponent {
-
+  esMovil(): boolean {
+    return /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+  }
+  
+  llamar() {
+    let telefono: string = document.getElementById('numero')?.innerText || "";
+  
+    if (telefono) {
+      if (this.esMovil()) {
+        window.location.href = `tel:${telefono}`; // Llama en móvil
+      } else {
+        alert("Marcar por whatsapp web ")
+        window.open(`https://wa.me/${telefono}`, "_blank");
+      }
+    } else {
+      alert("Número no disponible");
+    }
+  }
 }
